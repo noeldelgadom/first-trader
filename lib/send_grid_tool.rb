fcd38946f5a6e7197a4fca14fea9e0139fa03fb3
Noel Delgado <noeldelgadom@gmail.com>
2020-06-06 18:44:46 -0500

    changed the email subject

--- lib/send_grid_tool.rb
+++ lib/send_grid_tool.rb
@@ -5,14 +5,15 @@ require 'sendgrid-ruby'
 include SendGrid
 
   def self.daily_trading_mail
-    from = Email.new(email: 'noeldelgadom@gmail.com')
-    to = Email.new(email: 'noeldelgadom@gmail.com')
-    subject = 'Sending with SendGrid is Fun'
-    content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-    mail = Mail.new(from, subject, to, content)
+    me        = 'noeldelgadom@gmail.com'
+    from      = Email.new(email: me)
+    to        = Email.new(email: me)
+    subject   = 'Daily Trading Email: ' + Date.today.to_s
+    content   = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+    mail      = Mail.new(from, subject, to, content)
 
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    sg        = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    response  = sg.client.mail._('send').post(request_body: mail.to_json)
     puts response.status_code
     puts response.body
     puts response.headers
